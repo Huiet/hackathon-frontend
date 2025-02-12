e00ff5499666ab6d451c0fe49cf108ee5ca75778
ssines23 <ssines@lumafintech.com>
2025-02-12 15:27:23 -0500

    Temp commit.

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { IconEdit, IconHome2, IconUser } from "@tabler/icons-react";
+import { IconEdit, IconHome2, IconUser, IconInfoCircle } from "@tabler/icons-react";
 import { Stack, Tooltip, UnstyledButton } from "@mantine/core";
 import classes from "./Navbar.module.scss";
 import { Link } from "@tanstack/react-router";
@@ -14,50 +14,45 @@ interface NavbarLinkProps {
 function NavbarLink({ icon: Icon, label, route }: NavbarLinkProps) {
   const exact = !route.includes("edit-policies");
   return (
-    <Tooltip label={label} position="right" transitionProps={{ duration: 0 }}>
-      <UnstyledButton
-        component={Link}
-        to={route}
-        activeOptions={{ exact }}
-        className={classes.link}
-        activeProps={{
-          className: classes.link_active,
-        }}
-      >
-        <Icon size={20} stroke={2} />
-      </UnstyledButton>
-    </Tooltip>
+      <Tooltip label={label} position="right" transitionProps={{ duration: 0 }}>
+        <UnstyledButton
+            component={Link}
+            to={route}
+            activeOptions={{ exact }}
+            className={classes.link}
+            activeProps={{
+              className: classes.link_active,
+            }}
+        >
+          <Icon size={20} stroke={2} />
+        </UnstyledButton>
+      </Tooltip>
   );
 }
 
 const routeLinks = [
   { icon: IconHome2, label: "Users", route: "/" },
   { icon: IconUser, label: "User Policies", route: "/$userId" },
-  {
-    icon: IconEdit,
-    label: "Edit My Policies",
-    route: "/$userId/edit-policies/",
-  },
+  { icon: IconEdit, label: "Edit My Policies", route: "/$userId/edit-policies/" },
+  { icon: IconInfoCircle, label: "About", route: "/about" }, // New About button
 ];
 
 export function Navbar({ userId }: { userId: string | number | null }) {
-  // const params = Route.useParams();
-  // const userId = params.userId;
   const filteredLinks =
-    userId != null
-      ? routeLinks
-      : routeLinks.filter((link) => link.label === "Users");
+      userId != null
+          ? routeLinks
+          : routeLinks.filter((link) => link.label === "Users");
   const links = filteredLinks.map((link, index) => (
-    <NavbarLink key={link.label} {...link} />
+      <NavbarLink key={link.label} {...link} />
   ));
 
   return (
-    <nav className={classes.navbar}>
-      <div className={classes.navbarMain}>
-        <Stack justify="center" gap={0}>
-          {links}
-        </Stack>
-      </div>
-    </nav>
+      <nav className={classes.navbar}>
+        <div className={classes.navbarMain}>
+          <Stack justify="center" gap={0}>
+            {links}
+          </Stack>
+        </div>
+      </nav>
   );
 }
